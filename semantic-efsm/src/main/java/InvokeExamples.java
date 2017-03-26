476f8ce238f9aca94f20b9221bdcd46d15d34e74
Manuel Leduc <manuel.leduc@inria.fr>
2017-03-26 16:17:06 +0200

    wip

---
+++ semantic-efsm/src/main/java/InvokeExamples.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mleduc on 19/03/17.
+ */
+public class InvokeExamples {
+    public static void main(String[] args) {
+        InvokeExamples sc = new InvokeExamples();
+        sc.run();
+    }
+
+    private void run() {
+        List<String> ls = new ArrayList<>();
+        ls.add("Good Day");
+
+        ArrayList<String> als = new ArrayList<>();
+        als.add("Dydh Da");
+    }
+}
